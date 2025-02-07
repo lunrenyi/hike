4e1b5d6567fc656e96d8b7c5d577dee818382918
Dave Pearson <davep@davep.org>
2025-02-07 10:26:00 +0000

    :sparkles: Add the __init__ file for the whole application

--- src/hike/__init__.py
+++ src/hike/__init__.py
@@ -1,2 +1,17 @@
-def hello() -> str:
-    return "Hello from hike!"
+"""Hike -- A Markdown viewer for the terminal."""
+
+##############################################################################
+# Python imports.
+from importlib.metadata import version
+
+##############################################################################
+# Main app information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2025, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = version("hike")
+__licence__ = "GPLv3+"
+
+### __init__.py ends here
