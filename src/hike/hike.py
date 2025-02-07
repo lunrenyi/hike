db1789d196f3bcd59f65ed998c33064e0cf1f111
Dave Pearson <davep@davep.org>
2025-02-07 10:29:17 +0000

    :sparkles: Add the scaffold for the application

---
+++ src/hike/hike.py
@@ -0,0 +1,13 @@
+"""The main application class."""
+
+##############################################################################
+# Textual enhanced imports.
+from textual_enhanced.app import EnhancedApp
+
+
+##############################################################################
+class Hike(EnhancedApp[None]):
+    """The main application class."""
+
+
+### hike.py ends here
