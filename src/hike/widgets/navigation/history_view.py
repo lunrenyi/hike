68beaab1e1aacbe2532df089421590c760d12e52
Dave Pearson <davep@davep.org>
2025-02-09 22:42:41 +0000

    :sparkles: Flesh out the history list a little more
    Also add a simple form of selecting something from history. This will need
    improving as it's just a placeholder for the moment. For one thing, when you
    visit somewhere from history a fresh copy goes to the top of history, which
    isn't what you'd expect.
    
    I also think that I should collapse OpenFile and OpenURL into one message,
    at this point, called OpenLocation or similar. And then I should probably
    have ReopenLocation that will do similar but not add to history.
    
    I think I might also have to change Viewer so that it either doesn't use a
    reactive to specify the location to visit, but instead uses a method -- or
    keep it and add a goto_history method that takes a history ID and changes
    things around under the hood (actually this might be the better approach).

--- src/hike/widgets/navigation/history_view.py
+++ src/hike/widgets/navigation/history_view.py
@@ -1,12 +1,64 @@
 """Provides the history for the navigation panel."""
 
 ##############################################################################
+# Python imports.
+from pathlib import Path
+
+##############################################################################
+# Rich imports.
+from rich.text import Text
+
+##############################################################################
+# Textual imports.
+from textual import on
+from textual.widgets.option_list import Option
+
+##############################################################################
 # Textual-enhanced imports.
 from textual_enhanced.widgets import EnhancedOptionList
 
 ##############################################################################
 # Local imports.
-from ...types import HikeHistory
+from ...messages import OpenFile, OpenURL
+from ...types import HikeHistory, HikeLocation
+
+
+##############################################################################
+class Location(Option):
+    """A location within the history."""
+
+    def __init__(self, location_id: int, location: HikeLocation) -> None:
+        """Initialise the location object.
+
+        Args:
+            location_id: The ID of the location within the history.
+            location: The location.
+        """
+        self.location_id = location_id
+        """The ID of the location within the history."""
+        self.location = location
+        """The location."""
+        super().__init__(
+            Text.from_markup(
+                f":page_facing_up: [bold]{location.name}[/]\n[dim]{location.parent}[/]",
+                overflow="ellipsis",
+            )
+            if isinstance(location, Path)
+            else Text.from_markup(
+                f":globe_with_meridians: [bold]{Path(location.path).name}[/]"
+                f"\n[dim]{Path(location.path).parent}\n{location.host}[/]",
+                overflow="ellipsis",
+            )
+        )
+
+    @property
+    def open_request(self) -> OpenFile | OpenURL:
+        """A message that will request the location is opened."""
+        return (
+            OpenFile(self.location)
+            if isinstance(self.location, Path)
+            else OpenURL(self.location)
+        )
 
 
 ##############################################################################
@@ -30,7 +82,21 @@ class HistoryView(EnhancedOptionList):
             history: The history to update with.
         """
         with self.preserved_highlight:
-            self.clear_options().add_options(str(item) for item in history)
+            self.clear_options().add_options(
+                Location(location_id, location)
+                for location_id, location in reversed(list(enumerate(history)))
+            )
+
+    @on(EnhancedOptionList.OptionSelected)
+    def visit_from_history(self, message: EnhancedOptionList.OptionSelected) -> None:
+        """Visit a location from history.
+
+        Args:
+            message: The message to say which option was selected.
+        """
+        message.stop()
+        assert isinstance(message.option, Location)
+        self.post_message(message.option.open_request)
 
 
 ### history.py ends here
