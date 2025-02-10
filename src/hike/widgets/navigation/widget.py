4970c7eeaad43c82966014af5f8fbbc5b1b2abce
Dave Pearson <davep@davep.org>
2025-02-10 22:21:56 +0000

    :fire: Remove an unused import

--- src/hike/widgets/navigation/widget.py
+++ src/hike/widgets/navigation/widget.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 
 ##############################################################################
 # Textual imports.
-from textual import on
 from textual.app import ComposeResult
 from textual.containers import Vertical
 from textual.reactive import var
