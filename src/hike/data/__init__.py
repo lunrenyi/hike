423b6aa75ad74fdd0d6522f39400fa60d3c63a74
Dave Pearson <davep@davep.org>
2025-02-10 15:17:07 +0000

    :sparkles: Add support code for saving and loading history

--- src/hike/data/__init__.py
+++ src/hike/data/__init__.py
@@ -8,13 +8,16 @@ from .config import (
     save_configuration,
     update_configuration,
 )
+from .history import load_history, save_history
 
 ##############################################################################
 # Exports.
 __all__ = [
     "Configuration",
     "load_configuration",
+    "load_history",
     "save_configuration",
+    "save_history",
     "update_configuration",
 ]
 
---
+++ src/hike/data/history.py
@@ -0,0 +1,65 @@
+"""Provides the code for saving and loading the history."""
+
+##############################################################################
+# Python imports.
+from json import dumps, loads
+from pathlib import Path
+from typing import Sequence
+
+##############################################################################
+# httpx imports.
+from httpx import URL
+
+##############################################################################
+# Local imports.
+from ..types import HikeHistory
+from .locations import data_dir
+
+
+##############################################################################
+def history_file() -> Path:
+    """Get the path for the history file.
+
+    Returns:
+        The path for the history file.
+    """
+    return data_dir() / "history.json"
+
+
+##############################################################################
+def save_history(history: HikeHistory) -> None:
+    """Save the history to storage.
+
+    Args:
+        history: The history to save.
+    """
+    history_file().write_text(
+        dumps(
+            [
+                ("url" if isinstance(entry, URL) else "path", str(entry))
+                for entry in history
+            ],
+            indent=4,
+        ),
+        encoding="utf-8",
+    )
+
+
+##############################################################################
+def load_history() -> HikeHistory:
+    """Load the history from storage.
+
+    Returns:
+        The loaded history.
+    """
+    return HikeHistory(
+        [
+            (URL if entry_type == "url" else Path)(entry)
+            for entry_type, entry in loads(history.read_text())
+        ]
+        if (history := history_file()).exists()
+        else []
+    )
+
+
+### history.py ends here
