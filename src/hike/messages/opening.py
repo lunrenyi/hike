ce9190fe3926372399d6999a71f2037cef65fa75
Dave Pearson <davep@davep.org>
2025-02-13 09:43:00 +0000

    :fire: Remove unused import

--- src/hike/messages/opening.py
+++ src/hike/messages/opening.py
@@ -4,7 +4,6 @@
 # Python imports.
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Final
 
 ##############################################################################
 # httpx imports.
