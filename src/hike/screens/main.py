03ca9aa4aee42ff502d79d6047c1046370fc3807
Dave Pearson <davep@davep.org>
2025-02-17 13:53:37 +0000

    :bulb: Leave a note for myself about defensive check_action

--- src/hike/screens/main.py
+++ src/hike/screens/main.py
@@ -336,6 +336,11 @@ class Main(EnhancedScreen[None]):
             `True` if it can perform, `False` or `None` if not.
         """
         if not self.is_mounted:
+            # Surprisingly it seems that Textual's "dynamic bindings" can
+            # cause this method to be called before the DOM is up and
+            # running. This breaks the rule of least astonishment, I'd say,
+            # but okay let's be defensive... (when I can come up with a nice
+            # little MRE I'll report it).
             return True
         if action == Forward.action_name():
             return self.query_one(Viewer).history.can_go_forward or None
